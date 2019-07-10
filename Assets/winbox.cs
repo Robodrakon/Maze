b0c20e900a4d1b89043638750915498535080f93
lukeDgreat <52707453+lukeDgreat@users.noreply.github.com>
2019-07-10 11:55:31 -0400

    sdfghjk

---
+++ Assets/winbox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class winbox : MonoBehaviour
+{
+    public int IsPlayer;
+
+    public void OnTriggerEnter(Collider other)
+    {
+       if(other.gameObject.layer == IsPlayer)
+        {
+            SceneManager.LoadScene("Maze 1");
+        }
+    }
+}
