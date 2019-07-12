5cf84b5d92c3f1f6a541b757d1b83a491dc0be6f
Robodrakon <52707415+Robodrakon@users.noreply.github.com>
2019-07-12 09:11:38 -0400

    Revert "sdga"
    This reverts commit 437b88e2ced47eedac36777174017cedce9a54c4.

---
+++ Assets/Winner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Winner : MonoBehaviour
+{
+    public int IsPlayer;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == IsPlayer)
+        {
+            SceneManager.LoadScene("main menu 1");
+        }
+    }
+
+
+
+
+
+
+
+
+}
